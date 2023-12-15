5571f124dab3772365530a3059d48245c154adac
David <dsxvqq@gmail.com>
2023-12-15 21:31:20 +0600

    created task class

--- Task.java
+++ Task.java
@@ -1,19 +1,77 @@
+import java.util.Date;
+
 public class Task {
-    private int id;
-    private String user_login;
+    private String userLogin;
+    private int taskId;
     private String name;
     private String description;
+    private Date creationDate;
+    private Date deadline;
     private String status;
-    private String creationDate;
-    private String deadlineDate;
 
-    public Task(int id, String user_login, String name, String description, String status, String creationDate, String deadlineDate) {
-        this.id = id;
-        this.user_login = user_login;
+    public Task(String userLogin, int taskId, String name, String description, Date creationDate, Date deadline, String status) {
+        this.userLogin = userLogin;
+        this.taskId = taskId;
         this.name = name;
         this.description = description;
-        this.status = status;
         this.creationDate = creationDate;
-        this.deadlineDate = deadlineDate;
+        this.deadline = deadline;
+        this.status = status;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
